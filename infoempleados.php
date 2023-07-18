38178a774d26ada15cefa3ef827b54c7b2566cc0
CRISSTOOFFER <131405005+CRISSTOOFFER@users.noreply.github.com>
2023-07-18 16:03:23 -0500

    Update infoempleados.php

--- infoempleados.php
+++ infoempleados.php
@@ -7,10 +7,14 @@ $Identificacion= $_POST ['Identificacion'];
 $NomApellidos=$_POST ['NomApellidos'];
 $Cargo=$_POST ['Cargo'];
 $Salario=$_POST ['Salario'];
-echo "La identificacion del Empleado es: ".Identificacion;
-echo "Los nombres y apellidos del empleado son: ".NomApellidos;
-echo "El cargo del empleado es: ".Cargo;
-echo "El salario del empleado es:  ".Salario;
+echo "La identificacion del Empleado es: ".$Identificacion;
+echo "<br>"
+echo "Los nombres y apellidos del empleado son: ".$NomApellidos;
+echo "<br>"
+echo "El cargo del empleado es: ".$Cargo;
+echo "<br>"
+echo "El salario del empleado es:  ".$Salario;
+echo "<br>"
 ?>'
 </body>
 </head>
